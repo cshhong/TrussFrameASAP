9fce2fb934416f5cf3a2fae5eca077b68dd7e990
soohwahong <soohwahong@gmail.com>
2025-05-24 22:54:45 -0400

    add asap to julia env

--- julia_env_configure.py
+++ julia_env_configure.py
@@ -6,6 +6,6 @@ jl = juliacall.newmodule("TrussFrameRL")
 print(jl.seval('Base.active_project()'))
 
 jl.seval('using Pkg')
-# jl.seval('Pkg.add("Asap")')
+jl.seval('Pkg.add("Asap")')
 
-jl.seval('import Pkg; Pkg.add("AsapToolkit")')
+# jl.seval('import Pkg; Pkg.add("AsapToolkit")')
