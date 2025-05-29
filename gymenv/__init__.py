2730f1f21538e7305e354c11b5a12519669fba04
soohwahong <soohwahong@gmail.com>
2025-05-29 14:51:59 -0400

    don't have to register v0, v1

--- gymenv/__init__.py
+++ gymenv/__init__.py
@@ -6,20 +6,20 @@ from gymnasium.envs.registration import register
 
 def register_env():
     # Import inside the function to avoid circular import issues
-    from .cantileverenv_v0 import CantileverEnv_0
-    from .cantileverenv_v1 import CantileverEnv_1
+    # from .cantileverenv_v0 import CantileverEnv_0
+    # from .cantileverenv_v1 import CantileverEnv_1
     from .cantileverenv_v2 import CantileverEnv_2
 
-    register(
-        id="Cantilever-v0",
-        entry_point="gymenv.cantileverenv_v0:CantileverEnv_0",
-        max_episode_steps=1000,
-    )
-    register(
-        id="Cantilever-v1",
-        entry_point="gymenv.cantileverenv_v1:CantileverEnv_1",
-        max_episode_steps=1000,
-    )
+    # register(
+    #     id="Cantilever-v0",
+    #     entry_point="gymenv.cantileverenv_v0:CantileverEnv_0",
+    #     max_episode_steps=1000,
+    # )
+    # register(
+    #     id="Cantilever-v1",
+    #     entry_point="gymenv.cantileverenv_v1:CantileverEnv_1",
+    #     max_episode_steps=1000,
+    # )
     register(
         id="Cantilever-v2",
         entry_point="gymenv.cantileverenv_v2:CantileverEnv_2",
